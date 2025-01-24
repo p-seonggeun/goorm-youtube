eded4c1475491a19759398477664fdb28e571ef0
topolo.edu <topolo.edu@gmail.com>
2025-01-25 04:21:36 +0900

    프로필 및 몇가지 작업

---
+++ src/main/java/io/goorm/youtube/mapper/MemberMapper.java
@@ -0,0 +1,28 @@
+package io.goorm.youtube.mapper;
+
+import io.goorm.youtube.domain.Member;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+
+    List<Member> selectAll();
+
+    Member selectById(@Param("memberSeq") Long memberSeq);
+
+    Member selectByMemberId(@Param("memberId") String memberId);
+
+    boolean existsById(String memberId);
+
+    int insert(@Param("member") Member member);
+
+    int update(@Param("member") Member member);
+
+    int updateUseYn(@Param("member") Member member);
+
+    int delete(@Param("memberSeq") Long memberSeq);
+
+}
---
+++ src/main/java/io/goorm/youtube/service/MemberService.java
@@ -0,0 +1,58 @@
+package io.goorm.youtube.service;
+
+import io.goorm.youtube.mapper.MemberMapper;
+import io.goorm.youtube.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Slf4j
+@org.springframework.stereotype.Service
+public class MemberService {
+
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Autowired
+    public MemberService(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
+
+
+    public List<Member> findAll() {
+
+        return memberMapper.selectAll();
+    }
+
+    public Member login(Member member) {
+
+        return memberMapper.selectByMemberId(member.getMemberId());
+    }
+
+    public Member find(Long memberSeq) {
+
+        return memberMapper.selectById(memberSeq);
+    }
+
+    public boolean existsById(String memberId) {
+        return memberMapper.existsById(memberId);
+    }
+
+    public int save(Member member) {
+
+        return memberMapper.insert(member);
+    }
+
+    public int update(Member member) {
+
+        return memberMapper.update(member);
+    }
+
+    public int updatePublishYn(Member member) {
+
+        return memberMapper.updateUseYn(member);
+    }
+
+}
